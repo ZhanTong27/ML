dfe50342a12d15482cbccf5ca1456d40933d1e1e
ZhanTong27 <86971285+ZhanTong27@users.noreply.github.com>
2026-07-13 03:29:22 +0200

    Use local V2.2 base data

--- career-roadmap/data.js
+++ career-roadmap/data.js
@@ -1,4 +1,4 @@
-document.write('<script src="https://cdn.jsdelivr.net/gh/ZhanTong27/ML@59f946ac9a7bb2ebdf495911a553e53c581e5cdb/career-roadmap/data.js"><\/script>');
+document.write('<script src="legacy-data-v22.js"><\/script>');
 document.write('<script src="curriculum/base.js"><\/script>');
 document.write('<script src="curriculum/cycle1.js"><\/script>');
 document.write('<script src="curriculum/cycle2.js"><\/script>');
